918c689216ad23a119a3f83d0d26634a5a790d8e
Elena <mikhnovetz.elena@yandex.ru>
2020-09-18 19:39:30 +0300

    add 5 task

---
+++ lab2task5/lab2task5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "movement out";
+           int[] array= new int[4] { 12, 3, 89, 7 };
+            
+            var result = GetResult(array, word);
+            Console.WriteLine(result);
+            Console.Read();
+        }
+
+        private static (int, int, string) GetResult(int[] array, string word)
+        {
+            int max = 0;
+            int min = 1000;
+            for(int i=0; i< 4; i++)
+            {
+                if(array[i]>max)
+                {
+                    max=array[i];
+                }
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                if (array[i] < min)
+                {
+                    min=array[i];
+                }
+            }
+            string newword = word.Remove(1,11);
+            var result = (max, min, newword);
+            return result;
+        }
+    }
+    
+}
+
